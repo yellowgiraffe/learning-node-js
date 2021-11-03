a5753774aae6277f06dcff34d7184f816b01ab7a
Olga Mirocz <ol.miroch@gmail.com>
2021-11-04 00:25:47 +0300

    add node-farm

---
+++ 1-node-farm.js/simpleWebServer.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// /////////// SERVER with very simple API
+// const server = http.createServer((req, res) => {
+//   const pathName = req.url;
+  
+//   if ( pathName === '/' || pathName === '/overview') {
+//     res.end('This is the OVERVIEW');
+//   } else if (pathName === '/product') {
+//     res.end('This is the PRODUCT')
+//   } else if (pathName === '/api') {
+//     fs.readFile(`${__dirname}/data.json`, 'utf-8', (err, data) => {
+//       const productData = JSON.parse(data);
+//       res.writeHead(200, {'Content-type': 'application/json'});
+//       res.end(data);
+//     });
+//   } else {
+//     res.writeHead(404, {
+//       'Content-type': 'text/html',
+//       'my-own-header': 'hello-world',
+//     });
+//     res.end('<h1>Page not found</h1>');
+//   }
+// });
+
+// server.listen(8000, '127.0.0.1', () => {
+//   console.log('Listening to requests on port 8000');
+// });
+
+/////////// SERVER with very simple API -- SYNC
+const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  
+  if ( pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT')
+  } else if (pathName === '/api') {
+    fs.readFile(`${__dirname}/data.json`, 'utf-8', (err, data) => {
+      res.writeHead(200, {'Content-type': 'application/json'});
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
