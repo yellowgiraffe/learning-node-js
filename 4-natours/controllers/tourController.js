8371ee5c7e7dcefc8df984118d36464a8bb3931e
Olga Mirocz <ol.miroch@gmail.com>
2021-11-06 00:08:20 +0300

    add chaining multiple middleware

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkID = (req, res, next, value) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price property',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -45,7 +55,7 @@ exports.createTour = (req, res) => {
   tours.push(newTour);
 
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
