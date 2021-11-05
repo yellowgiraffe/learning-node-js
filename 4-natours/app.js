563cedadf609d13f74755935e6bd741e238cc847
olga_mirocz <ol.miroch@gmail.com>
2021-11-05 19:06:00 +0300

    add our own middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,16 +1,34 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
+// MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// ROUTE HANDLERS
+
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -90,6 +108,8 @@ const deleteTour = (req, res) => {
   });
 };
 
+// ROUTS
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -98,6 +118,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// START SERVER
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
